3e3fc792c25fd933b63ddd90d1fad83f6af9ccf7
br_zhan <user@bIrzhan>
2025-04-01 02:38:20 +0500

    Project

---
+++ Web-Dev-Project/src/app/volunteer-list/volunteer-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-volunteer-list',
+  imports: [],
+  templateUrl: './volunteer-list.component.html',
+  styleUrl: './volunteer-list.component.css'
+})
+export class VolunteerListComponent {
+
+}
